054fa37d9af51c2669d6c292ba2ed144162a9dd7
Lysmi <lysmi.mx@mail.ru>
2022-09-15 02:34:02 +0200

    feat: interface update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets
-from pyqtgraph import PlotWidget
+from pyqtgraph import PlotWidget, PlotDataItem, mkBrush, mkColor, mkPen
 import sys  # We need sys so that we can pass argv to QApplication
 import mainForm
 import os
@@ -28,14 +28,22 @@ class MainWindow(QtWidgets.QMainWindow, mainForm.Ui_MainWindow):
                 self.v1_duration.value(),
                 self.upGraphicsView, self.downGraphicsView, self.v1_signaltype.currentText())
         )
-        self.upGraphicsView.setBackground(pyqtgraph.mkColor(0.82))
-        self.downGraphicsView.setBackground(pyqtgraph.mkColor(0.82))
-        self.upGraphicsView_2.setBackground(pyqtgraph.mkColor(0.82))
-        self.downGraphicsView_2.setBackground(pyqtgraph.mkColor(0.82))
-        self.upGraphicsView_3.setBackground(pyqtgraph.mkColor(0.82))
-        self.downGraphicsView_3.setBackground(pyqtgraph.mkColor(0.82))
-        self.upGraphicsView_4.setBackground(pyqtgraph.mkColor(0.82))
-        self.downGraphicsView_4.setBackground(pyqtgraph.mkColor(0.82))
+        self.upGraphicsView.setBackground(mkColor(0.82))
+        self.downGraphicsView.setBackground(mkColor(0.82))
+        self.upGraphicsView_2.setBackground(mkColor(0.82))
+        upGraphicsView2Legend = self.upGraphicsView_2.addLegend(offset=(2, 2),
+                                                                pen=mkPen(mkColor(0.0)), brush=mkBrush(mkColor(0.95)), labelTextColor=mkColor(0.0))
+        upGraphicsView2Legend.addItem(PlotDataItem(
+            range(100), pen=mkPen(color='b')), 'Data')
+        upGraphicsView2Legend.addItem(PlotDataItem(
+            range(100), pen=mkPen(color='r')), 'Filtered data')
+        self.downGraphicsView_2.setBackground(mkColor(0.82))
+        self.downGraphicsView_2.setTitle("Lowpass Filter Frequency Response")
+        self.upGraphicsView_3.addLegend()
+        self.upGraphicsView_3.setBackground(mkColor(0.82))
+        self.downGraphicsView_3.setBackground(mkColor(0.82))
+        self.upGraphicsView_4.setBackground(mkColor(0.82))
+        self.downGraphicsView_4.setBackground(mkColor(0.82))
 
 
 def secondExperiment(freq, duration, a1, a2, b1, b2, qtGraphUp, qtGraphDown, type):
@@ -66,21 +74,21 @@ def firstExperiment(freq, duration, qtGraphUp, qtGraphDown, type):
 
 def plotGraph(graph, qtGraph: PlotWidget):
     qtGraph.clear()
-    qtGraph.plot(graph, pen=pyqtgraph.mkPen(color='r'), symbol=None, )
+    qtGraph.plot(graph, pen=mkPen(color='r'), symbol=None, )
     qtGraph.autoRange()
 
 
 def plotGraphWithXY(graph, qtGraph: PlotWidget):
     qtGraph.clear()
-    qtGraph.plot(graph[0], graph[1], pen=pyqtgraph.mkPen(
+    qtGraph.plot(graph[0], graph[1], pen=mkPen(
         color='r'), symbol=None)
     qtGraph.autoRange()
 
 
 def plotGraphWithData(graph, data, qtGraph: PlotWidget):
     qtGraph.clear()
-    qtGraph.plot(graph, pen=pyqtgraph.mkPen(color='r'), symbol=None, )
-    qtGraph.plot(data, pen=pyqtgraph.mkPen(color='b'), symbol=None, )
+    qtGraph.plot(graph, pen=mkPen(color='r'), symbol=None, )
+    qtGraph.plot(data, pen=mkPen(color='b'), symbol=None, )
     qtGraph.autoRange()
 
 
